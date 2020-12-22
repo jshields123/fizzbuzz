f8a48bdfd00da96abe2fedef031d4ffd43516c2d
Sean Edwards <seanedwardsphysio@gmail.com>
2020-12-22 17:04:14 +0000

    refactor code for fizzbuzz method

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,11 +1,8 @@
 def fizzbuzz(number)
-  if number % 15 == 0
-    "fizzbuzz"
-  elsif number % 3 == 0
-    "fizz"
-  elsif number % 5 == 0
-    "buzz"
-  elsif number.is_a?(String)
-    "error"
+  case
+  when number % 15 == 0 then return "fizzbuzz"
+  when number % 3 == 0 then return "fizz"
+  when number % 5 == 0 then return "buzz"
+  when number.is_a?(String) then return "error"
   end
 end
